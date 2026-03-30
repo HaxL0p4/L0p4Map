0a299f8e74fd3bd61efe74294f9875facb761118
L0pa <haxl0p4@proton.me>
2026-03-30 08:33:08 +0200

    Update L0p4Map.sh
    Updated the L0p4Map launcher script

--- L0p4Map.sh
+++ L0p4Map.sh
@@ -1,1 +1,13 @@
+if ! command -v nmap >/dev/null 2>&1; then
+    if command -v apt >/dev/null 2>&1; then
+        sudo apt update
+        sudo apt install -y nmap
+    elif command -v pacman >/dev/null 2>&1; then
+        sudo pacman -Sy --noconfirm nmap
+    else
+        echo "Package manager not yet supported."
+        exit 1
+    fi
+fi
+
 sudo python3 ui/app.py
